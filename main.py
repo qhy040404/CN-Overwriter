d6385b9bd7cdb6c0f30c552192ad651d35ab3791
qhy040404 <qhy040404@163.com>
2025-06-10 18:46:47 +0800

    remove all upload

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
 import os
 import sys
-import threading
 
-import boto3
 import requests
-from botocore.config import Config
 
-from utils import download_file, rt_print
-
-
-class ProgressPercentage(object):
-    def __init__(self, filename):
-        self._filename = filename
-        self._size = float(os.path.getsize(filename))
-        self._seen_so_far = 0
-        self._lock = threading.Lock()
-
-    def __call__(self, bytes_amount):
-        with self._lock:
-            self._seen_so_far += bytes_amount
-            percentage = (self._seen_so_far / self._size) * 100
-            sys.stdout.write(
-                "\r%s  %s / %s  (%.2f%%)" % (
-                    self._filename, self._seen_so_far, self._size,
-                    percentage))
-            sys.stdout.flush()
-
+from utils import rt_print
 
 project = sys.argv[1]
 if project == "hutao":
@@ -33,19 +11,13 @@ if project == "hutao":
     github_release_api = f"https://api.github.com/repos/DGP-Studio/Snap.Hutao/releases/latest"
     release_asset_index = 1
     patch_url = "https://api.snapgenshin.com/patch/hutao"
-    download_file_name = "Snap.Hutao.msix"
-    need_download = False
     should_overwrite = True
-    cdn_mode = "preheat"
 elif project == "deployment":
     project_key = "snap-hutao-deployment"
     github_release_api = f"https://api.github.com/repos/DGP-Studio/Snap.Hutao.Deployment/releases/latest"
     release_asset_index = 0
     patch_url = "https://api.snapgenshin.com/patch/hutao-deployment"
-    download_file_name = "Snap.Hutao.Deployment.exe"
-    need_download = True
     should_overwrite = False
-    cdn_mode = "refresh"
 else:
     rt_print("Invalid project")
     sys.exit(1)
@@ -75,25 +47,6 @@ if should_overwrite:
                                "mirror_name": "GitHub Proxy",
                                "mirror_type": "direct"
                            }).text)
-if need_download:
-    rt_print("Downloading latest version")
-    download_file(asset_url, download_file_name)
-
-    rt_print("Starting upload...")
-    config = Config(signature_version='s3v4')
-    s3_client = boto3.client(
-        's3',
-        aws_access_key_id=os.getenv("S3_ACCESS_KEY"),
-        aws_secret_access_key=os.getenv("S3_SECRET_KEY"),
-        endpoint_url=os.getenv("S3_ENDPOINT"),
-        config=config
-    )
-    bucket_name = "hutao-distribute"
-    s3_client.upload_file(download_file_name, bucket_name, asset["name"],
-                          Callback=ProgressPercentage(download_file_name))
-    rt_print()
-    rt_print("Upload complete")
-if should_overwrite:
     rt_print("Add R2 to Patch API")
     rt_print(requests.post("https://api.snapgenshin.com/patch/mirror",
                            headers={
@@ -105,27 +58,3 @@ if should_overwrite:
                                "mirror_name": "Cloudflare R2",
                                "mirror_type": "direct"
                            }).text)
-
-rt_print("Preheating CDN Caches...")
-if need_download:
-    rt_print("Preheating MinIO")
-    minio_s3_client = boto3.client(
-        's3',
-        aws_access_key_id=os.getenv("MINIO_ACCESS_KEY"),
-        aws_secret_access_key=os.getenv("MINIO_SECRET_KEY"),
-        endpoint_url=os.getenv("MINIO_ENDPOINT"),
-        config=config
-    )
-    minio_bucket_name = "hutao"
-    minio_s3_client.upload_file(download_file_name, minio_bucket_name, asset["name"],
-                                Callback=ProgressPercentage(download_file_name))
-    rt_print()
-    rt_print("Preheating complete")
-rt_print(requests.get(f"https://api.qhy04.com/hutaocdn/{cdn_mode}?filename={asset["name"]}", headers={
-    "Authorization": os.getenv("CDN_TOKEN")
-}).text)
-
-if os.path.exists("Snap.Hutao.msix"):
-    os.remove("Snap.Hutao.msix")
-if os.path.exists("Snap.Hutao.Deployment.exe"):
-    os.remove("Snap.Hutao.Deployment.exe")
--- utils.py
+++ utils.py
@@ -6,20 +6,3 @@ import requests
 # 实时打印
 def rt_print(message=""):
     print(message, flush=True)
-
-
-# 下载文件
-def download_file(url, path):
-    with closing(requests.get(url, stream=True)) as response:
-        chunk_size = 10240  # 单次请求最大值
-        content_size = int(response.headers['content-length'])  # 内容体总大小
-        data_count = 0
-        progresses = []
-        with open(path, "wb") as file:
-            for data in response.iter_content(chunk_size=chunk_size):
-                file.write(data)
-                data_count = data_count + len(data)
-                progress = int(data_count / content_size * 100)
-                if progress not in progresses:
-                    progresses.append(progress)
-                    rt_print(f"Progress: {progress} %")
